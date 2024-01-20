ed762d9eccde664af12e9e03680d51d4a37a9382
Hernan Arevalo <hernanarevalo16@gmail.com>
2024-01-19 21:13:29 -0300

    Upload send email format

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import Script from 'next/script'
+// import Script from 'next/script'
  
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Script src="https://smtpjs.com/v3/smtp.js" />
+      {/* <Script src="https://smtpjs.com/v3/smtp.js" /> */}
     </>
   )
 }
--- sections/5_ContactSection/ContactSection.jsx
+++ sections/5_ContactSection/ContactSection.jsx
@@ -28,22 +28,6 @@ export const ContactSection = ({info}) => {
     onResetForm()
   }
 
-//   const sendEmail = () => {
-
-    // Email.send({
-    //     Host : "smtp.elasticemail.com",
-    //     Username : "username",
-    //     Password : "password",
-    //     To : 'hernanarevalo16@gmail.com',
-    //     From : "you@isp.com",
-    //     Subject : "This is the subject",
-    //     Body : "And this is the body"
-    // }).then(
-    //   message => alert(message)
-    // );
-
-//   }
-
   return (
     <>
 
