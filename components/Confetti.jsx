d871ed017089c7d45c6ed1d14aa5a24b34f4bf43
Hernán Arévalo <hernanarevalo16@gmail.com>
2024-06-25 16:08:45 -0300

    format document and add LikedOnSession from store

--- components/Confetti.jsx
+++ components/Confetti.jsx
@@ -1,23 +1,22 @@
-"use client"
+'use client';
 import ReactConfetti from 'react-confetti';
 import { useSelector } from 'react-redux';
 import { useWindowSize } from 'react-use';
 
 export const Confetti = () => {
-
-  const {liked} = useSelector(state => state.likes)
-  const { width, height,  } = useWindowSize()
- 
+  const { liked, likedOnSession } = useSelector((state) => state.likes);
+  const { width, height } = useWindowSize();
 
   return (
     <div className="confetti fixed w-full h-full pointer-events-none">
-      { liked &&
-        <ReactConfetti colors={['#FFF7D6', '#00B2FF']} 
-                       recycle={false} 
-                      height={height}
-                      width={width-20}
+      {likedOnSession && liked && (
+        <ReactConfetti
+          colors={['#FFF7D6', '#00B2FF']}
+          recycle={false}
+          height={height}
+          width={width - 20}
         />
-      }
+      )}
     </div>
   );
 };
