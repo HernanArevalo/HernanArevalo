eb0aa6d4f46844eee89fc65bf75c79ebb7a9255e
Hernán Arévalo <hernanarevalo16@gmail.com>
2025-07-31 16:15:57 -0300

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,3 +1,3 @@
-export const linkedin_URL = "https://www.linkedin.com/in/hernan-arevalo"
+export const linkedin_URL = "https://www.linkedin.com/in/HernanArevalo"
 export const github_URL = "https://www.github.com/HernanArevalo"
 export const email = "hernanarevalo16@gmail.com"
